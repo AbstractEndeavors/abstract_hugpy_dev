3895d0323bc6ba167cb5bca4fff8d3fb2326ed91
solcatcher <jrputkey55@gmail.com>
2026-06-26 20:04:30 -0500

    baseline: abstract_hugpy_dev 0.1.56 (from PyPI sdist)

---
+++ setup.py
@@ -0,0 +1,35 @@
+from time import time
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='abstract_hugpy_dev',
+    version='0.1.56',
+    author='putkoff',
+    author_email='partners@abstractendeavors.com',
+    description='The `abstract_hugpy_dev` module is designed to facilitate hugging face modules',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/AbstractEndeavors/abstract_hugpy_dev',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,          # honor MANIFEST.in for the wheel too
+    package_data={
+        "abstract_hugpy_dev": ["**/*.sh"],   # belt-and-suspenders: explicit glob
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=['abstract_utilities','requests','openai_whisper','abstract_flask'],
+    python_requires=">=3.6",
+    # Add this line to include wheel format in your distribution
+    setup_requires=['wheel']
+)
